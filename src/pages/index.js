d8975cf146c602a4fc25f5147404776b8034e72a
Anna Goltsman <goltsman.anna@gmail.com>
2025-01-30 21:39:01 +0200

    fix import

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import "../pages/index.css";
 import Card from "../components/Card.js";
 import FormValidator from "../components/FormValidator.js";
 import Section from "../components/Section.js";
-import PopupWithImage from "../components/Popup.js";
+import PopupWithImage from "../components/PopupWithImage.js";
 import PopupWithForm from "../components/PopupWithForm.js";
 import UserInfo from "../components/UserInfo.js";
 import { initialCards, validationSettings } from "../utils/utils.js";
